f8df4650f7b39ba6009c96ebee974aac9d103d5b
dasmcode <mrinmoydas0604@gmail.com>
2026-08-24 21:39:09 +0530

    Add python lsp server for nvim

--- nvimconfig/.config/nvim/lua/lsp.lua
+++ nvimconfig/.config/nvim/lua/lsp.lua
@@ -1,18 +1,20 @@
 require("mason").setup()
 
 require("mason-lspconfig").setup({
-    ensure_installed = {
-        "lua_ls",
+	ensure_installed = {
+		"lua_ls",
 		"yamlls",
-        "basedpyright",
-        "ruff",
-        "clangd",
-        "bashls",
-        "jsonls",
-    },
+		"basedpyright",
+		"ruff",
+		"clangd",
+		"bashls",
+		"jsonls",
+		"pylsp",
+	},
 })
 
 vim.lsp.enable({
+	"pylsp",
 	"lua_ls",
 	"yamlls",
 	"basedpyright",
