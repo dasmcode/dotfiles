e3b4cefbf8caa3d71192966800836057dfb3e228
dasmcode <mrinmoydas0604@gmail.com>
2026-09-11 17:05:09 +0530

    Add navigation support for herdr and nvim and make bufferline minimal

--- nvimconfig/.config/nvim/lua/plugins/bufferline.lua
+++ nvimconfig/.config/nvim/lua/plugins/bufferline.lua
@@ -3,13 +3,13 @@ return {
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
 	opts = {
-		options = {
-			mode = "buffers",
-			separator_style = "slant",
-			themable = true,
-			diagnostics = "nvim_lsp",
-			color_icons = true,
-		},
+		-- options = {
+		-- 	mode = "buffers",
+		-- 	separator_style = "slant",
+		-- 	themable = true,
+		-- 	diagnostics = "nvim_lsp",
+		-- 	color_icons = true,
+		-- },
 		highlights = require("catppuccin.special.bufferline").get_theme(),
 	},
 }
---
+++ nvimconfig/.config/nvim/lua/plugins/herdr-nvim.lua
@@ -0,0 +1,7 @@
+return {
+	"aimdevlee/herdr-nvim-nav",
+	dependencies = { "christoomey/vim-tmux-navigator" }, -- omit if with_tmux = false
+	config = function()
+		require("herdr-nvim-nav").setup()
+	end,
+}
